ba5961b53b30994c9964281e131bfabd2197c683
ikoba12 <ikoba12@freeuni.edu.ge>
2015-06-03 18:39:46 +0300

    Print response codes to console

---
+++ src/main/java/ge/freeuni/sdp/xo/bot/App.java
@@ -0,0 +1,42 @@
+package ge.freeuni.sdp.xo.bot;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class App {
+	private static String[] names={"login","signin","chat","rooms","game","achiev","history"};
+	private static String before="http://xo-";
+	private static String after=".herokuapp.com/webapi/ping";
+	public static int getStatusForServiceName(String serviceName) throws IOException{
+		String url=before+serviceName+after;
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+ 		con.setRequestMethod("GET");
+		return con.getResponseCode();
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		for(int i=0;i<names.length;i++){
+			int responseCode = getStatusForServiceName(names[i]);
+			System.out.println("Host: " + before+names[i]+after);
+			System.out.println("Response Code : " + responseCode);
+		}
+		
+ 
+		/*BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+ 
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+ 
+		//print result
+		System.out.println(response.toString());*/
+	}
+}
