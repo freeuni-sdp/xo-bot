98bc824e83d8caaca938baac04b843fb9a5fce9a
ikoba12 <ikoba12@freeuni.edu.ge>
2015-06-03 19:23:55 +0300

    now using Client class

--- src/main/java/ge/freeuni/sdp/xo/bot/App.java
+++ src/main/java/ge/freeuni/sdp/xo/bot/App.java
@@ -1,28 +1,32 @@
 package ge.freeuni.sdp.xo.bot;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.jackson.JacksonFeature;
 
 public class App {
 	private static String[] names={"login","signin","chat","rooms","game","achiev","history"};
 	private static String before="http://xo-";
 	private static String after=".herokuapp.com/webapi/ping";
 	//returns response code for ping
-	public static int getStatusForServiceName(String serviceName) throws IOException{
-		
+	public static int getStatusForServiceName(String serviceName,Client c) throws IOException{
 		String url=before+serviceName+after;
-		URL obj = new URL(url);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
- 		con.setRequestMethod("GET");
-		return con.getResponseCode();
+		Response r=c.target(url).request().get();
+		return r.getStatus();
 		
 	}
 	
 	public static void main(String[] args) throws IOException {
+		ClientConfig config = new ClientConfig().register(JacksonFeature.class);
+		Client client = ClientBuilder.newClient(config);
 		for(int i=0;i<names.length;i++){
-			int responseCode = getStatusForServiceName(names[i]);
+			int responseCode = getStatusForServiceName(names[i],client);
 			System.out.println("Host: " + before+names[i]+after);
 			System.out.println("Response Code : " + responseCode);
 		}
